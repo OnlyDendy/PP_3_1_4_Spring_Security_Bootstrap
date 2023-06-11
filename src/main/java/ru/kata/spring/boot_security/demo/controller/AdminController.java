479dd6ccd32767a01de84c40c2f1ae3126a04735
AntonShorin <Antnshorin2007@gmail.com>
2023-06-11 19:47:54 +0300

    Ведутся работы

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,11 +1,10 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
@@ -24,42 +23,38 @@ public class AdminController {
         this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping
-    public String toUsers() {
+    @GetMapping()
+    public String findAll(Model model ,@AuthenticationPrincipal User user) {
+        model.addAttribute("user", user);
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("allRoles", roleService.findAll());
         return "admin";
     }
 
-    @GetMapping("/users")
-    public String findAll(Model model) {
-        model.addAttribute("users", userService.findAll());
-        model.addAttribute("allRoles", roleService.findAll());
-        return "users";
-    }
-
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     public String findById(Model model, @PathVariable Long id) {
         model.addAttribute("user", userService.findById(id));
         model.addAttribute("allRoles", roleService.findAll());
         return "user";
     }
 
-    @PostMapping("/users")
+    @PostMapping()
     public String add(User user, String rawPassword) {
         user.setPassword(passwordEncoder.encode(rawPassword));
         userService.add(user);
-        return "redirect:/admin/users";
+        return "redirect:/admin";
     }
 
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/{id}")
     public String delete(@PathVariable Long id) {
         userService.removeById(id);
-        return "redirect:/admin/users";
+        return "redirect:/admin";
     }
 
-    @PutMapping("/users/{id}")
+    @PutMapping("/{id}")
     public String update(User user, String rawPassword) {
         user.setPassword(passwordEncoder.encode(rawPassword));
         userService.update(user);
-        return "redirect:/admin/users";
+        return "redirect:/admin";
     }
 }
